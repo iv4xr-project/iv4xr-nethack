38de49e12ce9a6e3d754bf2811459d88b79224a9
GerardvSchie <g.j.vanschie@students.uu.nl>
2023-01-24 11:15:25 +0100

    FFT: Kill previous WSL process automatically

--- server-python/wsl-start.bat
+++ server-python/wsl-start.bat
@@ -1,1 +1,5 @@
+REM Kill previous instance of this bat file
+taskkill /F /FI "WindowTitle eq WSL NetHack" /T>nul
+REM Title the instance so it can be identified later
+TITLE WSL NetHack>nul
 wsl ~ -d Ubuntu-22.04 -e bash -c "bash /mnt/c/Users/gerard/MegaDrive/Documents/M2-Thesis/Code/iv4xr-nethack/server-python/start.sh"
