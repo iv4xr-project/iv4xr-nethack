8bc2e59fcf82224c34caed4a65f76c165631e81c
GerardvSchie <g.j.vanschie@students.uu.nl>
2023-03-05 17:18:46 +0100

    ADD: Visualize coverage per method

--- util-python/src/coverage.py
+++ util-python/src/coverage.py
@@ -6,97 +6,49 @@ import numpy as np
 
 
 def get_entries_from_data(data: json) -> [(str, int)]:
-    entries = [(f['filename'], f['line_total'], f['line_covered']) for f in data['files']]
+    entries = [(f['filename'].replace('src/', ''), f['line_total'], f['line_covered']) for f in data['files']]
     sorted_entries = sorted(entries, key=lambda x: (x[1], x[0]), reverse=True)
     return sorted_entries
 
 
-def create_line_coverage_plot(iv4xr_file_path: str, bothack_file_path: str):
+def create_line_coverage_plot(file_path: str, method: str, show_file_names=False):
     # Get the data
-    with open(iv4xr_file_path, 'r') as f:
-        iv4xr_data = json.load(f)
+    with open(file_path, 'r') as f:
+        data = json.load(f)
 
-    with open(bothack_file_path, 'r') as f:
-        bothack_data = json.load(f)
-
-    xy1 = get_entries_from_data(iv4xr_data)
-    xy2 = get_entries_from_data(bothack_data)
-
-    # Get the labels and percentages for the bars
-    file_labels1, line_total1, line_amount1 = zip(*xy1)
-    file_labels2, line_total2, line_amount2 = zip(*xy2)
-
-    print(f'NetHack 3.6.6 ({len(file_labels1)}): ', sorted(file_labels1))
-    print(f'NetHack 3.4.3 nao ({len(file_labels2)}): ', sorted(file_labels2))
-    sys.exit()
+    # Get the data per file
+    xy = get_entries_from_data(data)
+    file_labels, line_total, line_amount = zip(*xy)
+    print(method, f'({len(file_labels)}):', sorted(file_labels))
 
     # Set up the bar chart
-    fig, ax = plt.subplots(figsize=(25, 4))
-    index = np.arange(len(file_labels1))
-    bar_width = 0.35
+    if show_file_names:
+        fig, ax = plt.subplots(figsize=(19, 6))
+    else:
+        fig, ax = plt.subplots(figsize=(7, 4))
 
-    # Plot the bars for line coverage
-    ax.bar(index, line_amount1, bar_width, label='Line Coverage iv4XR', color='red')
-    # ax.bar(index, line_amount2, bar_width, label='Line Coverage BotHack', color='blue')
-    ax.plot(index, line_total1, label='Nr lines', color='black')
+    index = np.arange(len(file_labels))
+    ax.bar(index, line_amount, label='Nr. lines covered')
+    ax.plot(index, line_total, label='Total nr. lines', color='black')
 
     # Add the labels and legends
     ax.set_ylabel('Line Coverage')
     ax.set_xlabel('Files')
-    ax.set_title('Coverage by File')
-    ax.set_xticks(index)
-    ax.set_xticklabels(file_labels1, horizontalalignment='right', rotation=45)
-    ax.legend()
+    ax.set_title(f'Per-file coverage using {method}')
 
+    if show_file_names:
+        ax.set_xticks(index)
+        ax.set_xticklabels(file_labels, horizontalalignment='right', rotation=45)
+    else:
+        ax.set_xticks(index)
+        ax.set_xticklabels([])
+
+    ax.legend()
     os.makedirs("plots", exist_ok=True)
 
     fig.tight_layout()
     fig.align_labels()
-    fig.savefig("plots/coverage.png")
-
-
-
-# def create_line_coverage_plot(iv4xr_file_name: str, bothack_file_name: str):
-#     # Get the data for the two types of coverage
-#     # branch_data = [(f['filename'], f['branch_covered']) for f in data['files']]
-#     line_data = [(f['filename'], f['line_covered']) for f in data['files']]
-#
-#     # Sort the data by percentage in descending order
-#     # branch_data = sorted(branch_data, key=lambda x: x[1], reverse=True)
-#     line_data = sorted(line_data, key=lambda x: x[1], reverse=True)
-#
-#     # Get the labels and percentages for the bars
-#     # branch_labels, branch_percentages = zip(*branch_data)
-#     line_labels, line_percentages = zip(*line_data)
-#
-#     # Set up the bar chart
-#     fig, (ax1, ax2) = plt.subplots(nrows=2, sharex=True, figsize=(25, 8))
-#     index = np.arange(len(branch_labels))
-#     bar_width = 0.35
-#
-#     # Plot the bars for branch coverage
-#     ax1.bar(index, branch_percentages, bar_width, label='Branch Coverage', color='blue')
-#
-#     # Plot the bars for line coverage
-#     ax2.bar(index, line_percentages, bar_width, label='Line Coverage', color='red')
-#
-#     # Add the labels and legends
-#     ax1.set_ylabel('Branch Coverage')
-#     ax2.set_ylabel('Line Coverage')
-#     ax2.set_xlabel('Files')
-#     ax1.set_title('Coverage by File')
-#     ax1.set_xticks(index)
-#     ax1.set_xticklabels(branch_labels, horizontalalignment='right', rotation=45)
-#     ax2.set_xticks(index)
-#     ax2.set_xticklabels(line_labels, horizontalalignment='right', rotation=45)
-#     ax1.legend()
-#     ax2.legend()
-#
-#     os.makedirs("plots", exist_ok=True)
-#
-#     fig.tight_layout()
-#     fig.align_labels()
-#     fig.savefig("plots/coverage.png")
+    fig.savefig(f"plots/coverage_{method}.png")
 
 
 def get_coverage_file_names(directory_path: str) -> [str]:
@@ -116,9 +68,10 @@ def get_coverage_file_names(directory_path: str) -> [str]:
 
 def main():
     iv4xr_coverage_names = get_coverage_file_names('../server-python/coverage')
-    bothack_coverage_names = get_coverage_file_names('../BotHack/coverage')
+    create_line_coverage_plot(iv4xr_coverage_names[0], 'iv4XR', True)
 
-    create_line_coverage_plot(iv4xr_coverage_names[0], bothack_coverage_names[0])
+    bothack_coverage_names = get_coverage_file_names('../BotHack/coverage')
+    create_line_coverage_plot(bothack_coverage_names[0], 'BotHack', True)
 
 
 if __name__ == '__main__':
