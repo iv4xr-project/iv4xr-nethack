eb4a204fa061bb9a6a7c957ecff86ea312573f43
GerardvSchie <g.j.vanschie@students.uu.nl>
2023-03-08 13:10:20 +0100

    FFT: Automatically collect coverage from commandline

--- BotHack/start.sh
+++ BotHack/start.sh
@@ -10,8 +10,26 @@ trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 BASEDIR=$(cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)
 cd "$BASEDIR"
 
+# Check if the first argument is either 'coverage' or 'no-coverage'
+if [ "$1" != "coverage" ] && [ "$1" != "no-coverage" ]; then
+  echo "Invalid argument: '$1'. Must be 'coverage' or 'no-coverage'."
+  exit 1
+fi
+
 cp bothack.nethackrc /nh343/.nethackrc
+
+if [ "$1" = "coverage" ] ; then
+  sh reset-coverage.sh
+elif [ "$1" = "no-coverage" ]; then
+  echo ALERT: Coverage will not be collected over this individual run
+  sleep 10
+fi
+
 LD_LIBRARY_PATH=jta26/jni/linux/ lein run config/shell-config.edn
+
+if [ "$1" = "coverage" ] ; then
+    sh coverage.sh
+fi
 
 # Remove trap
 trap - EXIT
