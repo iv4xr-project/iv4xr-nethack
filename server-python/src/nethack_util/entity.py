a693cbcb60a50339da8bb20faeb7f43550f85d49
GerardvSchie <g.j.vanschie@students.uu.nl>
2023-04-09 23:05:52 +0200

    CRF: Entity information about skills in JSON

--- server-python/src/nethack_util/entity.py
+++ server-python/src/nethack_util/entity.py
@@ -18,6 +18,8 @@ def filtered_object_properties():
 def object_info():
     filtered_properties = filtered_object_properties()
 
+    launchers = {21, 22, 23}
+
     object_infos = []
     for i in range(nethack.NUM_OBJECTS):
         object_info = nethack.objclass(i)
@@ -25,6 +27,7 @@ def object_info():
         object_dict["index"] = i
         object_dict["name"] = nethack.OBJ_NAME(object_info)
         object_dict["description"] = nethack.OBJ_DESCR(object_info)
+
         for property in filtered_properties:
             mapped_property = property
             if property == "oc_name_idx" or property == "oc_descr_idx" or property == "oc_oprop" or property == "oc_class" or property == "oc_delay" or property == "oc_color" or property == "oc_prob":
@@ -35,13 +38,108 @@ def object_info():
             elif property == "oc_cost":
                 mapped_property = "cost"
             elif property == "oc_subtyp":
-                mapped_property = "subtype"
+                value = object_info.__getattribute__(property)
+                abs_value = abs(value)
+                object_dict["skill"] = toSkill(abs_value)
+                if value == 0:
+                    continue
+
+                is_missile = value < 0
+                if is_missile:
+                    object_dict["missile"] = True
+                if launchers.__contains__(abs_value):
+                    object_dict["fromLauncher"] = True
+                continue
 
             object_dict[mapped_property] = object_info.__getattribute__(property)
 
         object_infos.append(object_dict)
 
     return object_infos
+
+
+def toSkill(value: int):
+    match value:
+        case 0:
+            return "NONE"
+        case 1:
+            return "DAGGER"
+        case 2:
+            return "KNIFE"
+        case 3:
+            return "AXE"
+        case 4:
+            return "PICK_AXE"
+        case 5:
+            return "SHORT_SWORD"
+        case 6:
+            return "BROAD_SWORD"
+        case 7:
+            return "LONG_SWORD"
+        case 8:
+            return "TWO_HANDED_SWORD"
+        case 9:
+            return "SCIMITAR"
+        case 10:
+            return "SABER"
+        case 11:
+            return "CLUB"
+        case 12:
+            return "MACE"
+        case 13:
+            return "MORNING_STAR"
+        case 14:
+            return "FLAIL"
+        case 15:
+            return "HAMMER"
+        case 16:
+            return "QUARTERSTAFF"
+        case 17:
+            return "POLEARMS"
+        case 18:
+            return "SPEAR"
+        case 19:
+            return "TRIDENT"
+        case 20:
+            return "LANCE"
+        case 21:
+            return "BOW"
+        case 22:
+            return "SLING"
+        case 23:
+            return "CROSSBOW"
+        case 24:
+            return "DART"
+        case 25:
+            return "SHURIKEN"
+        case 26:
+            return "BOOMERANG"
+        case 27:
+            return "WHIP"
+        case 28:
+            return "UNICORN_HORN"
+        case 29:
+            return "ATTACK_SPELL"
+        case 30:
+            return "HEALING_SPELL"
+        case 31:
+            return "DIVINATION_SPELL"
+        case 32:
+            return "ENCHANTMENT_SPELL"
+        case 33:
+            return "CLERIC_SPELL"
+        case 34:
+            return "ESCAPE_SPELL"
+        case 35:
+            return "MATTER_SPELL"
+        case 36:
+            return "BARE_HANDED_COMBAT"
+        case 37:
+            return "TWO_WEAPON_COMBAT"
+        case 38:
+            return "RIDING"
+        case 39:
+            return "NUM_SKILLS"
 
 
 def write_object_info():
