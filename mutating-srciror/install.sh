9d51310704cfbef0ce44cfc937fa5ceef1cdb8c9
GerardvSchie <g.j.vanschie@students.uu.nl>
2023-04-23 22:00:44 +0200

    DOC: gitignore CRF: llvm-build script

---
+++ mutating-srciror/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# e: exit if fails, x: trace
+set -e
+
+# shellcheck disable=SC2154
+# echo an error message before exiting
+trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+
+# Run script from script directory
+BASEDIR=$(cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)
+cd "$BASEDIR"
+
+sh ./llvm-build.sh
+cd SRCMutation
+make
+
+# Remove trap
+trap - EXIT
--- mutating-srciror/llvm-build.sh
+++ mutating-srciror/llvm-build.sh
@@ -1,6 +1,7 @@
-#! /bin/bash -e
+#!/bin/bash
 
-# ================= Main Body ===================== #
+export PYTHON=/usr/bin/python3
+
 ZIPS_DIR=zips
 TOOLS_DIR=tools
 LLVM_DIR=llvm
@@ -13,9 +14,9 @@ wget -N http://llvm.org/releases/3.8.1/compiler-rt-3.8.1.src.tar.xz -P $ZIPS_DIR
 wget -N http://llvm.org/releases/3.8.1/cfe-3.8.1.src.tar.xz -P $ZIPS_DIR/
 
 # unzip
-tar xfJ  $ZIPS_DIR/llvm-3.8.1.src.tar.xz
-tar xfJ  $ZIPS_DIR/compiler-rt-3.8.1.src.tar.xz
-tar xfJ  $ZIPS_DIR/cfe-3.8.1.src.tar.xz
+tar xfJ $ZIPS_DIR/llvm-3.8.1.src.tar.xz
+tar xfJ $ZIPS_DIR/compiler-rt-3.8.1.src.tar.xz
+tar xfJ $ZIPS_DIR/cfe-3.8.1.src.tar.xz
 
 # relocate the sources
 rm -rf $LLVM_DIR
@@ -23,10 +24,18 @@ mv llvm-3.8.1.src $LLVM_DIR
 mv compiler-rt-3.8.1.src $LLVM_DIR/projects/compiler-rt
 mv cfe-3.8.1.src $LLVM_DIR/tools/clang
 
+# Fix compilation error in llvm
+input_file=$LLVM_DIR/include/llvm/IR/ValueMap.h
+temp_file="ValueMap_temp.h"
+search_pattern="bool hasMD() const { return MDMap; }"
+replacement_string="bool hasMD() const { return bool(MDMap); }"
+sed "s/$search_pattern/$replacement_string/g" "$input_file" > "$temp_file"
+mv "$temp_file" "$input_file"
+
 # build llvm
-rm -rf  $LLVM_BUILD
+rm -rf $LLVM_BUILD
 mkdir $LLVM_BUILD
-cd $LLVM_BUILD
+cd $LLVM_BUILD || exit
 export C_INCLUDE_PATH=
 export CPLUS_INCLUDE_PATH=
 ../llvm/configure
