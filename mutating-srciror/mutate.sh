bd24c9e5eabf17d04f974dc2538db3b76225d23e
GerardvSchie <g.j.vanschie@students.uu.nl>
2023-04-24 10:10:33 +0200

    UFT: Mutate script in mutate folder

--- mutating-srciror/mutate.sh
+++ mutating-srciror/mutate.sh
@@ -10,6 +10,9 @@ trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 BASEDIR=$(cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)
 cd "$BASEDIR"
 
+# Delete mutate files
+find .. -name "*.mut.*" -delete
+
 # Function to expand a range of numbers
 expand_range() {
     local start end range result
@@ -80,7 +83,6 @@ export SRCIROR_SRC_MUTATOR=$BASEDIR/SRCMutation/build/mutator
 
 # List of paths
 paths=(
-    "../src/share"
     "$NETHACK_DIR/include"
     "$NETHACK_DIR/src"
     "$NETHACK_DIR/sys/unix"
@@ -90,36 +92,20 @@ paths=(
     "$NETHACK_DIR/build/src"
     "$NETHACK_DIR/build/util"
     "/usr/include"
-    # "/usr/lib/gcc/x86_64-linux-gnu/11/"
 )
 
 # Initialize include and library options
 include_opts=""
-library_opts=""
 
 # Loop through the paths and construct the options
 for path in "${paths[@]}"; do
     # Check if the path is a directory and exists
     if [[ -d "$path" ]]; then
         include_opts+=" -I${path}"
-        library_opts+=" -L${path}"
     fi
 done
 
-# Add library paths
-library_opts+=" -L/usr/lib/gcc/x86_64-linux-gnu/11/"
-library_opts+=" -L/usr/lib/x86_64-linux-gnu/"
-
-# Print the include and library options
-echo "Include options: ${include_opts}"
-echo "Library options: ${library_opts}"
-
-# # Example clang command with include and library options
-# clang_cmd="clang -c myfile.c ${include_opts} ${library_opts} -o myfile.o"
-# echo "Example clang command: ${clang_cmd}"
-
-python3 $BASEDIR/PythonWrappers/mutationClang $FILE_PATH -B/usr/lib/gcc/x86_64-linux-gnu/11/ ${include_opts} ${library_opts}
-# python3 $BASEDIR/PythonWrappers/mutationClang $FILE_PATH --sysroot=/usr/lib/gcc/x86_64-linux-gnu/12 -L"$NETHACK_DIR"/src -I"$NETHACK_DIR"/src -I"$NETHACK_DIR"/src/share -I"$NETHACK_DIR"/sys/unix -I"$NETHACK_DIR"/win/tty -I"$NETHACK_DIR"/win/rl -I"$NETHACK_DIR"/include -I"$NETHACK_DIR"/build/include -I"$NETHACK_DIR"/build/src -I/usr/include -I/usr/lib/gcc/x86_64-linux-gnu/12/ -L"$NETHACK_DIR"/src/share -L"$NETHACK_DIR"/sys/unix -L"$NETHACK_DIR"/win/tty -L"$NETHACK_DIR"/win/rl -L"$NETHACK_DIR"/include -L"$NETHACK_DIR"/build/include -L"$NETHACK_DIR"/build/src -L/usr/include -L/usr/lib/gcc/x86_64-linux-gnu/12/ -o test -v
+python3 $BASEDIR/PythonWrappers/mutationClang $FILE_PATH ${include_opts}
 
 # Remove trap
 trap - EXIT
